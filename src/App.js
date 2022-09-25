264e88ea82f4ef56aff5895ce90d5a4d5c491d37
onkar <onkar271998@gmail.com>
2022-09-25 12:06:55 +0530

    translator app done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
-import Navbar from './Navbar';
+import logo from "./logo.svg";
+import "./App.css";
+import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer } from 'react-toastify';
+import { Navbar } from './components/Navbar';
+import { TranslateBox } from './components/TranslateBox';
 
 function App() {
   return (
     <div className="App">
-    
+      <Navbar />
+      <TranslateBox />
+      <ToastContainer autoClose={3000} />
     </div>
   );
 }
